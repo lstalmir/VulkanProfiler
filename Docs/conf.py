dd8543886d16d1f7a671ba7c902f3b2f408fabc1
Lukasz Stalmirski <lucasus1129@gmail.com>
2025-06-18 00:11:26 +0200

    Add versioning support.

--- Docs/conf.py
+++ Docs/conf.py
@@ -13,8 +13,15 @@ author = 'Łukasz Stalmirski'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autosectionlabel', 'sphinx_tabs.tabs']
+extensions = ['sphinx.ext.autosectionlabel', 'sphinx_tabs.tabs', 'sphinx_multiversion']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for versioning --------------------------------------------------
+smv_tag_whitelist = r'^.*$'
+smv_branch_whitelist = r'^master|docs$'
+smv_remote_whitelist = 'origin'
+smv_outputdir_format = '{ref.name}'
+smv_prefer_remote_refs = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -22,3 +29,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_style = 'css/style.css'
 html_static_path = ['static']
+templates_path = ['templates']
