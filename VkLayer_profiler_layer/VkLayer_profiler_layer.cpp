8feb541351bc4aed720d3ace31724ed348def565
Lukasz Stalmirski <lucasus1129@gmail.com>
2025-10-19 22:34:42 +0200

    Export required instance functions to load the layer on Android.

--- VkLayer_profiler_layer/VkLayer_profiler_layer.cpp
+++ VkLayer_profiler_layer/VkLayer_profiler_layer.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2019-2021 Lukasz Stalmirski
+﻿// Copyright (c) 2019-2025 Lukasz Stalmirski
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,16 +23,17 @@
 #include "profiler_layer_functions/core/VkDevice_functions.h"
 #include <vulkan/vk_layer.h>
 
+// clang-format off
 #undef VK_LAYER_EXPORT
-#if defined( _MSC_VER )
-#   define VK_LAYER_EXPORT extern "C"
-#elif defined( __GNUC__ ) && __GNUC__ >= 4
-#   define VK_LAYER_EXPORT extern "C" __attribute__((visibility("default")))
-#elif defined(__SUNPRO_C) && (__SUNPRO_C >= 0x590)
-#   define VK_LAYER_EXPORT extern "C" __attribute__((visibility("default")))
+#if (defined(__GNUC__) && (__GNUC__ >= 4)) || \
+    (defined(__clang__)) || \
+    (defined(__SUNPRO_C) && (__SUNPRO_C >= 0x590))
+#  define VK_LAYER_EXPORT extern "C" __attribute__((visibility("default")))
 #else
-#   define VK_LAYER_EXPORT extern "C"
+// MSVC compiler uses def file for export definitions.
+#  define VK_LAYER_EXPORT extern "C"
 #endif
+// clang-format on
 
 /***************************************************************************************\
 
@@ -64,6 +65,41 @@ VK_LAYER_EXPORT PFN_vkVoidFunction VKAPI_CALL vkGetDeviceProcAddr(
     const char* name )
 {
     return Profiler::VkDevice_Functions::GetDeviceProcAddr( device, name );
+}
+
+/***************************************************************************************\
+
+Function:
+    vkEnumerateInstanceLayerProperties
+
+Description:
+    Entrypoint to the vkEnumerateInstanceLayerProperties.
+    Loader on Android requires this function to be exported directly from the SO.
+
+\***************************************************************************************/
+VK_LAYER_EXPORT VkResult VKAPI_CALL vkEnumerateInstanceLayerProperties(
+    uint32_t* pPropertyCount,
+    VkLayerProperties* pProperties )
+{
+    return Profiler::VkInstance_Functions::EnumerateInstanceLayerProperties( pPropertyCount, pProperties );
+}
+
+/***************************************************************************************\
+
+Function:
+    vkEnumerateInstanceExtensionProperties
+
+Description:
+    Entrypoint to the vkEnumerateInstanceExtensionProperties.
+    Loader on Android requires this function to be exported directly from the SO.
+
+\***************************************************************************************/
+VK_LAYER_EXPORT VkResult VKAPI_CALL vkEnumerateInstanceExtensionProperties(
+    const char* pLayerName,
+    uint32_t* pPropertyCount,
+    VkExtensionProperties* pProperties )
+{
+    return Profiler::VkInstance_Functions::EnumerateInstanceExtensionProperties( pLayerName, pPropertyCount, pProperties );
 }
 
 #ifdef WIN32
