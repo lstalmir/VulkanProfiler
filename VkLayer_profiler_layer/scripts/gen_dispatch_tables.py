09eb5989f2e4dbedbf56e4b9260ea4efa3e7e207
Lukasz Stalmirski <lucasus1129@gmail.com>
2026-02-08 17:13:44 +0100

    Use mock ICD from Vulkan-Tools for testing.

--- VkLayer_profiler_layer/scripts/gen_dispatch_tables.py
+++ VkLayer_profiler_layer/scripts/gen_dispatch_tables.py
@@ -24,7 +24,7 @@ import io
 import xml.etree.ElementTree as etree
 
 # Read configuration variables
-VULKAN_HEADERS_DIR = os.path.abspath( sys.argv[ 1 ] )
+VULKAN_REGISTRY_XML_PATH = os.path.abspath( sys.argv[ 1 ] )
 CMAKE_CURRENT_BINARY_DIR = os.path.abspath( sys.argv[ 2 ] )
 
 class VulkanSpec:
@@ -155,8 +155,7 @@ class DispatchTableGenerator:
 # Generate dispatch tables
 def gen_dispatch_tables():
     out_path = os.path.join( CMAKE_CURRENT_BINARY_DIR, "vk_dispatch_tables.h" )
-    vk_xml_path = os.path.join( VULKAN_HEADERS_DIR, "registry", "vk.xml" )
-    vk_xml = etree.parse( vk_xml_path )
+    vk_xml = etree.parse( VULKAN_REGISTRY_XML_PATH )
     generator = DispatchTableGenerator( vk_xml )
     with open( out_path, mode="w" ) as out:
         generator.write_commands( out )
